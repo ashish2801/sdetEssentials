3e4a696175fc9cc7c6b10608fbf78f1478499251
ashish2801 <ashishkmr.net@gmail.com>
2023-01-15 17:23:04 +0530

    final commit

--- src/test/java/com/sdet/sdetPractise/docker/RemoteGridWithDocker1Test.java
+++ src/test/java/com/sdet/sdetPractise/docker/RemoteGridWithDocker1Test.java
@@ -1,17 +1,14 @@
 package com.sdet.sdetPractise.docker;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class RemoteGridWithDocker1 extends DockerBaseTest {
+public class RemoteGridWithDocker1Test extends DockerBaseTest {
 	
 
 	@Test
